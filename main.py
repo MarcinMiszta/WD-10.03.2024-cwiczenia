7af6057898bbd5a02da4afb341d64dd5ea1fd4b2
MarcinMiszta <157218780+MarcinMiszta@users.noreply.github.com>
2024-03-10 13:42:23 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# try:
+#     a = int(input())
+#     b = int(input())
+#     result = a/b
+#     # print(result)
+# # except ZeroDivisionError:
+# # except ValueError:
+#     # print("wrong value")
+# except Exception:
+#     print("error")
+# else:
+#     print(result)
+
+l1 = [1,2,3,4,5,6,7,8,9]
+l2 = []
+for i in l1:
+    l2.append(i**2)
+print(l2)
+l2 = [i**2 for i in l1]
+print(l2)
+l3 = [3**y for y in range(1, 6)]
+print(l3)
+l4 = [x for x in l1 if x % 2 == 1]
+print(l4)
+l5 = [(x, y) for x in l2 for y in l4]
+print(l5)
+l6 = []
+for i in l2:
+    for j in l3:
+        l6.append((i, j))
+print(l6)
+
+
