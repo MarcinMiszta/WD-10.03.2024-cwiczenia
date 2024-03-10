097c67b0169286618caee17d59d967a8f3387cf7
MarcinMiszta <157218780+MarcinMiszta@users.noreply.github.com>
2024-03-10 15:20:56 +0100

    zad5

--- zad3.py
+++ zad3.py
@@ -1,3 +1,3 @@
 zakupy = {'chleb': 'sztuki', 'ser': 'kg', 'mleko': 'litry'}
-zakupy2 = {key: value for key, value in zakupy.items() if value == 'sztuki'}
+zakupy2 = [key for key, value in zakupy.items() if value == 'sztuki']
 print(zakupy2)
---
+++ zad5.py
@@ -0,0 +1,6 @@
+def pole(a=12,b=2,h=10):
+    p = 1/2 * (a + b) * h
+    return p
+
+
+print(pole(5,4,5))
